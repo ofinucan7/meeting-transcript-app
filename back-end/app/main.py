40248a68172b55c41c315ab148d9cdbfb05c236d
Owen Finucan <74834467+ofinucan7@users.noreply.github.com>
2025-12-24 18:21:50 -0500

    Update main.py

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -21,7 +21,7 @@ for o in ["http://localhost:5173", "http://127.0.0.1:5173"]:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allow_origins,
+    allow_origins=["https://frontend-production-7a77.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
